f9cd641d123c137960249e210dac8f219dba0eb7
Rhod-lab <55310735+Rhod-lab@users.noreply.github.com>
2025-05-18 15:42:56 +0000

    adding project files

--- Makefile
+++ Makefile
@@ -2,11 +2,11 @@ install:
 	pip install --upgrade pip &&\
 		pip install -r requirements.txt
 test:
-	pytest -m pytest -vv --cov=hello test_*.py
+	python -m pytest -vv --cov=CalCLI --cov=mylib test_*.py
 format:
-	black *.py
+	black *.py mylib/*.py
 lint:
-	pylint --disable=R,C --ignore-patterns=test_.*?.py *.py
+	pylint --disable=R,C --ignore-patterns=test_.*?py *.py mylib/*.py
 container:
 	docker run -rm -i hadolint/hadolint < Dockerfile
 refactor: format lint
---
+++ mylib/calc.py
@@ -0,0 +1,29 @@
+"""
+A calculator module
+"""
+
+
+def add(a, b):
+    """Add two numbers"""
+    return a + b
+
+
+def sub(a, b):
+    """subtract two numbers"""
+    return a - b
+
+
+def mul(a, b):
+    """multiply two numbers"""
+    return a * b
+
+
+def div(a, b):
+    """divide two numbers"""
+    return a / b
+
+
+# build a function to calculate the power of a number with x as the base and y as the exponent
+def power(a, b):
+    """Power of a number"""
+    return a**b
